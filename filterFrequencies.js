10fc32b6154096ccc48748fdfa76ba1f4cb95919
archiemartini <archie.cgm@gmail.com>
2022-10-25 13:31:09 +0100

    refactored logic into map() function

--- filterFrequencies.js
+++ filterFrequencies.js
@@ -1,20 +1,9 @@
 const filterFrequencies = (frequencyArray, lowerLimit = 40, upperLimit = 1000) => {
-  let lengthOfArray = frequencyArray.length
 
-  if(lengthOfArray == 1) {
-    frequencyArray[0] = filterFrequency(frequencyArray[0], lowerLimit, upperLimit)
-    return frequencyArray
-  } else if(lengthOfArray == 2) {
-    frequencyArray[0] = filterFrequency(frequencyArray[0], lowerLimit, upperLimit)
-    frequencyArray[1] = filterFrequency(frequencyArray[1], lowerLimit, upperLimit)
-    return frequencyArray
-  } else if (lengthOfArray == 3) {
-    frequencyArray[0] = filterFrequency(frequencyArray[0], lowerLimit, upperLimit)
-    frequencyArray[1] = filterFrequency(frequencyArray[1], lowerLimit, upperLimit)
-    frequencyArray[2] = filterFrequency(frequencyArray[2], lowerLimit, upperLimit)
-    return frequencyArray
-    
-  }
+  return frequencyArray.map((frequency) => {
+    return filterFrequency(frequency, lowerLimit, upperLimit)
+  })
+
 }
 
 const filterFrequency = (frequency, lowerLimit, upperLimit) => {
