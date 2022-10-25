8f143b84dddea111e27762dd89324882c35b95a4
archiemartini <archie.cgm@gmail.com>
2022-10-25 13:13:25 +0100

    refactor implemented, frequencyArray returning

--- filterFrequencies.js
+++ filterFrequencies.js
@@ -1,11 +1,5 @@
 const filterFrequencies = (frequencyArray) => {
-  if(frequencyArray == 50) {
-    return [50]
-  } else if (frequencyArray == 100) {
-    return [100]
-  } else if (frequencyArray == 500) {
-    return [500]
-  }
+  return frequencyArray
 }
 
 module.exports = filterFrequencies;
