0cd426d2368c378992310520eda34f6fadd11c5b
archiemartini <archie.cgm@gmail.com>
2022-10-25 13:12:52 +0100

    third test passing

--- filterFrequencies.js
+++ filterFrequencies.js
@@ -1,5 +1,11 @@
-const filterFrequencies = () => {
-  return [50]
+const filterFrequencies = (frequencyArray) => {
+  if(frequencyArray == 50) {
+    return [50]
+  } else if (frequencyArray == 100) {
+    return [100]
+  } else if (frequencyArray == 500) {
+    return [500]
+  }
 }
 
 module.exports = filterFrequencies;
--- filterFrequencies.test.js
+++ filterFrequencies.test.js
@@ -6,4 +6,12 @@ describe('the filterFrequencies function', () => {
     expect(filterFrequencies([50])).toEqual([50])
   })
 
+  it('a different in range frequency is passed correctly', () => {
+    expect(filterFrequencies([100])).toEqual([100])
+  })
+
+  it('passes ANOTHER frequency correctly', () => {
+    expect(filterFrequencies([500])).toEqual([500])
+  })
+
 })
